dec1fec497671dd064763988b364ec1c3fe88b04
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-04-14 23:28:30 -0700

    renamed variables for sanity. updated html content in function, moved prompts to corresponding js files

--- index.js
+++ index.js
@@ -1,182 +1,118 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const filePath = "./dist/team.html";
-const Manager = require("./lib/manager");
-const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern");
+const teamPath = "teampage.html";
+const Manager = require("./lib/addManager");
+const Engineer = require("./lib/addEngineer");
+const Intern = require("./lib/addIntern");
 
-const html1 =
+const html =
     `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Sample page</title>
-</head>
-<body>
-    <div class = "jumbotron bg-danger">
-        <h1 class = "text-center text-light">
-            My Team
-        </h1>
-    </div>
-    <div class = "container-fluid">
-        <div class = "row">\n`;
-const html2 =
-    `        </div>
-</div>
-</body>
-</html>`;
-
-function addManager() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter manager name:",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Enter manager id:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Enter manager email:",
-        },
-        {
-            type: "input",
-            name: "officeNum",
-            message: "Enter manager office number:",
-        },
-    ])
-        .then((data) => {
-            const obj = new Manager(data.name, data.id, data.email, data.officeNum);
-            fs.appendFileSync(filePath, makeCard(obj));
-            askMember();
-        })
-};
-
-function addEngineer() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter engineer name:",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Enter engineer id:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Enter engineer email:",
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "Enter engineer GitHub username:",
-        },
-    ])
-        .then((data) => {
-            const obj = new Engineer(data.name, data.id, data.email, data.github);
-            fs.appendFileSync(filePath, makeCard(obj));
-            askMember();
-        })
-};
-
-function addIntern() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter intern name:",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Enter intern id:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Enter intern email:",
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "Enter intern school:",
-        },
-    ])
-        .then((data) => {
-            const obj = new Intern(data.name, data.id, data.email, data.school);
-            fs.appendFileSync(filePath, makeCard(obj));
-            askMember();
-        })
-};
-
-function makeCard(obj) {
-    const partOne = `            <div class = "col-2 px-2">
-                <div class = "card m-1 border-primary">
-                    <div class = "card-header bg-primary">
-                        <h4 class = "card-title text-light">${obj.getName()}</h4>
-                        <h5 class = "card-subtitle text-light">${obj.getRole()}</h5>
-                    </div>
-                    <div class = "card-body p-0">
-                        <ul class = "list-group list-group-flush">
-                            <li class = "list-group-item">ID: <span>${obj.getId()}</span></li>
-                            <li class = "list-group-item">Email: <a href = "mailto:${obj.getEmail()}">${obj.getEmail()}</a></li>\n`;
-
-    if (obj.getRole() === "Engineer") {
-        var partTwo = `                            <li class = "list-group-item">GitHub: <a href = "https://github.com/${obj.getGithub()}/" target = "_blank">${obj.getGithub()}</a></li>
-                        </ul>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <title>My Team!</title>
+    </head>
+    
+    <body>
+        <div class="jumbotron bg-secondary">
+            <h1 class="text-center text-light">
+                My Team
+            </h1>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-2 px-2">
+                    <div class="card m-1 border-dark">
+                        <div class="card-header bg-danger">
+                            <h4 class="card-title text-light">Bigboi</h4>
+                            <h5 class="card-subtitle text-light">Manager</h5>
+                        </div>
+                        <div class="card-body p-0">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: <span>1</span></li>
+                                <li class="list-group-item">Email: <a href="mailto:man@email.com">man@email.com</a></li>
+                                <li class="list-group-item">Office Number: <span>2</span></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>\n`;
-    } else if (obj.getRole() === "Manager") {
-        var partTwo = `                            <li class = "list-group-item">Office Number: <span>${obj.getOfficeNum()}</span></li>
-                        </ul>
+                <div class="col-2 px-2">
+                    <div class="card m-1 border-dark">
+                        <div class="card-header bg-danger">
+                            <h4 class="card-title text-light">Mediumlad</h4>
+                            <h5 class="card-subtitle text-light">Engineer</h5>
+                        </div>
+                        <div class="card-body p-0">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: <span>2</span></li>
+                                <li class="list-group-item">Email: <a href="mailto:medlad@email.com">Mediumlad@email.com</a>
+                                </li>
+                                <li class="list-group-item">GitHub: <a href="https://github.com/">GithubUsername</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>\n`;
-    } else {
-        var partTwo = `<li class = "list-group-item">School: <span>${obj.getSchool()}</span></li>
-                        </ul>
+                <div class="col-2 px-2">
+                    <div class="card m-1 border-dark">
+                        <div class="card-header bg-danger">
+                            <h4 class="card-title text-light">Smallboi</h4>
+                            <h5 class="card-subtitle text-light">Intern</h5>
+                        </div>
+                        <div class="card-body p-0">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: <span>3</span></li>
+                                <li class="list-group-item">Email: <a href="mailto:smol@boi.com">smol@boi.com</a></li>
+                                <li class="list-group-item">School: <span>schoolname</span></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>\n`;
-    }
+            </div>
+        </div>
+    </body>
+    
+    </html>`;
 
-    return partOne + partTwo;
-};
 
-function askMember() {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "choice",
-            message: "Add a new member?",
-            choices: ["Engineer", "Intern", "No"],
-        },
-    ])
-        .then((data) => {
-            if (data.choice === "No") {
-                fs.appendFileSync(filePath, html2);
-            } else if (data.choice === "Engineer") {
-                addEngineer();
-            } else {
-                addIntern();
-            }
-        })
-};
+// Generate card with info
+// ==========================
+
+// function showEmployeeCard(obj) {
+//     const partOne = divs from HTML for cards;
+//     return;
+// };
+
+
+// Ask for additional entries, if no, stop
+// ======================================
+
+// function askMember() {
+//     inquirer.prompt([
+//         {
+//             type: "list",
+//             name: "choice",
+//             message: "Add a new member?",
+//             choices: ["Engineer", "Intern", "No"],
+//         },
+//     ])
+//         .then((data) => {
+//             if (data.choice === "No") {
+//                 fs.appendFileSync(teamPath, html);
+//             } else if (data.choice === "Engineer") {
+//                 addEngineer();
+//             } else {
+//                 addIntern();
+//             }
+//         })
+// };
 
 function writeHTML() {
-    fs.writeFileSync(filePath, html1);
-    addManager();
+    fs.writeFileSync(teamPath, html);
 }
 
 writeHTML();
